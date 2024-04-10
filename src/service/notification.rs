f1af3f9c57584d6dceb3eebe267efc396965552c
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-04-10 17:36:44 +0700

    Tidy up and delete unused imports

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -4,10 +4,10 @@ use rocket::http::Status;
 use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
-use rocket::response::status::NotFound;
+
 
 use bambangshop_receiver::{APP_CONFIG, REQWEST_CLIENT, Result, compose_error_response};
-use crate::model::notification::{self, Notification};
+use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::repository::notification::NotificationRepository;
 
